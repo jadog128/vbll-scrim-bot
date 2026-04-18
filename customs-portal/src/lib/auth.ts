0f3bdc67026b2b8fdfdbb289507a3c89d3ddc76d
jadog128 <jamiedhend@gmail.com>
2026-04-18 15:02:19 +0100

    "Hardcode_Creds_Fix"

--- customs-portal/src/lib/auth.ts
+++ customs-portal/src/lib/auth.ts
@@ -5,8 +5,8 @@ import { isMemberAdmin } from "@/lib/discord";
 export const authOptions: NextAuthOptions = {
   providers: [
     DiscordProvider({
-      clientId: (process.env.VBLL_DISCORD_CLIENT_ID || process.env.DISCORD_CLIENT_ID) || "",
-      clientSecret: (process.env.VBLL_DISCORD_CLIENT_SECRET || process.env.DISCORD_CLIENT_SECRET) || "",
+      clientId: "1491103459942989905",
+      clientSecret: "d67TEmOW3O2GcsF6MwHJhsFMNmM0u6J2",
       authorization: { params: { scope: "identify email" } },
     }),
   ],
